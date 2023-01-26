50d89f9bcb4142aba7ed63981483d392f5397563
Cameron Griffin <cambackcountry@gmail.com>
2023-01-26 14:06:07 -0800

    add funtions

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let user = prompt("Your name:", "");
-document.write("Hello " + user  );
-
+// Intro function to greet new user
+let user = prompt("Your name:", ",");
+//function to ask if thier good and print out if or else
+window.onload = UserGreeting() {
+document.write("Hello " + user + " " );
 if (confirm("Confirm you're have a good day?")) {
-    document.write("Rightchous let's get on the ROCK!");
+    document.write("rightchous let's get on the ROCK!" + " ");
 } 
 else {
-    document.write("Have a better time climbing.");
+    document.write("have a better time climbing.");
 }
 
-document.write("My Name is Cam I'm stoked you showed today " + user  )
+document.write("My Name is Cam I'm stoked you showed up today " + user + ".")
+}
