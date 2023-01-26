93fdfc0b4a430fcc45ef77a93b558a487c661345
Cameron Griffin <cambackcountry@gmail.com>
2023-01-25 19:14:47 -0800

    add JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+let user = prompt("Your name:", "");
+document.write("Hello " + user  );
+
+if (confirm("Confirm you're have a good day?")) {
+    document.write("Rightchous let's get on the ROCK!");
+} 
+else {
+    document.write("Have a better time climbing.");
+}
+
+document.write("My Name is Cam I'm stoked you showed today " + user  )
