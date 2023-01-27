4fe1119e183bfdb8d8acc9763d85fc63f6dc7601
Cameron Griffin <cambackcountry@gmail.com>
2023-01-26 23:03:03 -0800

    add while loop

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-// Intro function to greet new user
+// Intro function to get new user name
 let user = prompt("Your name:", ",");
 
-//function to ask if thier good and print out if or else
+//function to ask if thier good and greet them
 function UserGreeting() {
 document.write("Hello " + user + " " );
 if (confirm("Confirm you're have a good day?")) {
-    document.write("rightchous let's get on the ROCK!" + " ");
+    document.write(",rightchous let's get on the ROCK!" + " ");
 } 
 else {
-    document.write("have a better time climbing.");
+    document.write(" have a better time climbing. ");
 }
 
-document.write("My Name is Cam I'm stoked you showed up today " + user + ".")
+document.write("My Name is Cam I'm stoked you showed up today " + user + ". ")
+}
+//Age question
+function UserAge() {
+    let userAge = prompt("Your Age:", " ");
+    if (userAge > 16) {
+        document.write("Awesome your old enough to climb." + " ");
+    } 
+    else if (userAge < 16) {
+        document.write(" I'm sorry you're " + (16-userAge) + " years too young to climb");
+    } 
+    else if(userAge != Number) {
+        document.write(" I'm not sure that's an age you gave me. ");
+    }
+}
+// Num Guess function
+
+let userNum =prompt("Pick a Number from 1-100: ", " ") 
+function NumGuess(){  
+i = 0
+    while (i < userNum) {
+        i++;
+}
+    document.write(" Your number was ", i);
 }
 
-window.onload = UserGreeting()
+
+UserGreeting();
+UserAge();
+NumGuess();
